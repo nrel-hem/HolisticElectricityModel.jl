1c4b3f3ddadc38096d8fb782d1d566ac990211c0
Nongchao Guo <43042596+Nongchao@users.noreply.github.com>
2022-01-03 16:42:20 -0700

    switch to gurobi solver

--- script/driver.jl
+++ script/driver.jl
@@ -4,14 +4,14 @@ using HolisticElectricityModel
 # This is the driver script
 
 # Define the solver ------------------------------------------------------------
-using Xpress
-MIP_solver = XpressSolver(Xpress)
+# using Xpress
+# MIP_solver = XpressSolver(Xpress)
 using Ipopt
 NLP_solver = Ipopt_Solver(Ipopt)
 
-# using Gurobi
-# const GRB_ENV = Gurobi.Env()
-# MIP_solver = Gurobi_Solver(Gurobi, GRB_ENV)
+using Gurobi
+const GRB_ENV = Gurobi.Env()
+MIP_solver = Gurobi_Solver(Gurobi, GRB_ENV)
 # ------------------------------------------------------------------------------
 
 # Define the model run ---------------------------------------------------------
