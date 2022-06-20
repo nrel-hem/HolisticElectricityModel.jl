2483871b09e2e17f5beeb97eeca072c8b0e06018
szhou <szhou@nrel.gov>
2022-06-20 11:57:19 -0600

    draft adding normalization

--- src/agents/distribution_utility.jl
+++ src/agents/distribution_utility.jl
@@ -1,3 +1,5 @@
+using TableTransforms
+
 mutable struct DistributionCostModel
     constant::ParamScalar
     saidi_coefficient::ParamScalar
@@ -86,16 +88,42 @@ function solve_agent_problem!(
 
     distribution_cost_before = regulator.distribution_cost
 
-    # TODO: (un)normalize regression
+    # Use static normalization numbers from the national database
+    norm_inputs = DataFrame(id = ["saidi", "dpv", "total_sales",
+                                  "residential", "commercial",
+                                  "industrial", "distribution_cost"],
+                            min = [0, 0.02, 33321,
+                                   3664, 820,
+                                   0, 1851740],
+                            max = [968.6, 3018.362, 131738016,
+                                   4489462, 619752,
+                                   33562,3236569353])
+    function norm(data, norm_variable)
+        data = (data - norm_inputs[norm_inputs.id .==norm_variable,"min"])/
+                (norm_inputs[norm_inputs.id .==norm_variable,"max"] - 
+                norm_inputs[norm_inputs.id .==norm_variable,"min"])
+        return data
+    end;
+                                
+    function reverse_norm(data, norm_variable)
+        data = data * (norm_inputs[norm_inputs.id .==norm_variable,"max"] - 
+                norm_inputs[norm_inputs.id .==norm_variable,"min"]) + 
+                norm_inputs[norm_inputs.id .==norm_variable,"min"]
+        return data
+    end;
+    # customer module updates customers.x_DG_E to be the DPV at the beginning of the year,
+    # to use end of the year, add customers.x_DG_new_my[reg_year_index, h, m]    
+    dpv_pca = sum(customers.x_DG_E[h, m] for h in model_data.index_h, m in customers.index_m)
+
     regulator.distribution_cost[reg_year_index] = 
-        distribution_cost_model.constant +
-        distribution_cost_model.saidi_coefficient * distribution_utility.SAIDI[reg_year_index] +
-        # customer module updates customers.x_DG_E to be the DPV at the beginning of the year, to use end of the year, add customers.x_DG_new_my[reg_year_index, h, m]
-        distribution_cost_model.dpv_coefficient * sum(customers.x_DG_E[h, m] for h in model_data.index_h, m in customers.index_m) +
-        distribution_cost_model.total_sales_coefficient * total_sale +
-        distribution_cost_model.residential_customer_coefficient * customers.gamma[:Residential] +
-        distribution_cost_model.commercial_customer_coefficient * customers.gamma[:Commercial] +
-        distribution_cost_model.industrial_customer_coefficient * customers.gamma[:Industrial]
+        reverse_norm(distribution_cost_model.constant +
+        distribution_cost_model.saidi_coefficient * norm(distribution_utility.SAIDI[reg_year_index], "saidi") +
+        distribution_cost_model.dpv_coefficient * norm(dpv_pca, "dpv")  +
+        distribution_cost_model.total_sales_coefficient * norm(total_sale, "total_sale") +
+        distribution_cost_model.residential_customer_coefficient * norm(customers.gamma[:Residential], "residential") +
+        distribution_cost_model.commercial_customer_coefficient * norm(customers.gamma[:Commercial], "commercial") +
+        distribution_cost_model.industrial_customer_coefficient * norm(customers.gamma[:Industrial], "industrial"),
+        "distribution_cost")
 
     return compute_difference_percentage_one_norm([
         (distribution_cost_before, regulator.distribution_cost),
