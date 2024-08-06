6e61093c6a22276b933fdab03d25f6bb2b1af424
Elaine Hale <elaine.hale@nrel.gov>
2024-08-06 16:18:09 -0600

    Completed config refactor proof of concept. Next step is to get parse_inputs working again.

--- script/hem.jl
+++ script/hem.jl
@@ -1,41 +1,77 @@
 using Revise
-using Combinatorics
-using Gurobi
-using Ipopt
-using JuMP
+# using Combinatorics
+# using Ipopt
+# using JuMP
 using YAML
 
 using HolisticElectricityModel
 import HolisticElectricityModelData
 
-const GUROBI_ENV = Gurobi.Env()
 const HEMDataRepo = HolisticElectricityModelData
+
+# Load config file
+if length(ARGS) > 0 # otherwise, define this variable in the REPL
+    config_fp = ARGS[0]
+end
+config = YAML.load_file(config_fp)
 
 # File locations
 base_dir = abspath(joinpath(dirname(Base.find_package("HolisticElectricityModel")), ".."))
 hem_data_dir = dirname(dirname(Base.find_package("HolisticElectricityModelData")))
 input_path = joinpath(hem_data_dir, "inputs")
-test_data_dir = joinpath(base_dir, "test", "driver_outputs")
+
+include(joinpath(base_dir, "script", "config.jl"))
+include(joinpath(base_dir, "script", "config_data.jl"))
+
+# ------------------------------------------------------------------------------
+# Input Folder and Data
+# ------------------------------------------------------------------------------
+
+balancing_areas, base_year, num_future_years, num_ipps, folder_suffix, skip_parse =
+    parse(config, "data_selection", validators)
+
+# Define scenario
+future_years = [base_year + i for i in range(1, num_future_years)]
+scenario = HEMDataRepo.DataSelection(balancing_areas, base_year, future_years, num_ipps)
+
+bal_areas_len = length(balancing_areas)
+input_dir_name =
+    "ba_" *
+    "$bal_areas_len" *
+    "_base_" *
+    "$base_year" *
+    "_future_" *
+    "$num_future_years" *
+    "_ipps_" *
+    "$num_ipps" *
+    "_" *
+    "$folder_suffix"
+input_dir = joinpath(hem_data_dir, "runs", input_dir_name)
+
+if !skip_parse
+    mkpath(input_dir)
+    # For the following to work, someone needs to have done the following from HolisticElectricityModelData:
+    #     - In julia: run(output_dir = PROFILES_DIRECTORY, user = "nguo", hostname = HOSTNAME, dbname = DATABASE, port = PORT, pca_ids = nothing) to get residential and commercial profiles
+    #     - In command prompt: python inputs/write_industrial_profiles.py #ba to get industrial profiles
+    HEMDataRepo.parse_inputs(input_path, input_dir, scenario, false)
+end
+
+error("Continue implementation here")
 
 # Acceptable options config_fields; maps config_fields to lists of acceptable options
 option_dict = Dict{String,Vector{Any}}()
-option_dict["balancing_areas"] = [
-    "p129",
-    "p130",
-    "p131",
-    "p132",
-    "p133",
-    "p134",
-]
+option_dict["balancing_areas"] = ["p129", "p130", "p131", "p132", "p133", "p134"]
 option_dict["base_year"] = collect(2018:2024)  # unsure if these base years are reasonable
 option_dict["num_future_years"] = collect(1:30)  # unsure if these horizons are reasonable
 option_dict["market_structure"] = ["wholesale_market", "vertically_integrated_utility"]
 option_dict["der_use_case"] = ["der_use_case", "null_use_case"]
 option_dict["supply_choice_use_case"] = ["supply_choice_use_case", "null_use_case"]
 option_dict["num_ipps"] = [1]  # unsure if more IPPs are desired
-option_dict["ipp_algorithm"] = ["lagrange_decomposition", "mppdcmer_transportation_storage", "mpppcmer", "miqp"]
+option_dict["ipp_algorithm"] =
+    ["lagrange_decomposition", "mppdcmer_transportation_storage", "mpppcmer", "miqp"]
 option_dict["rate_design"] = ["flat_rate", "time_of_use"]
-option_dict["net_metering_policy"] = ["excess_retail_rate", "excess_marginal_cost", "excess_zero"]
+option_dict["net_metering_policy"] =
+    ["excess_retail_rate", "excess_marginal_cost", "excess_zero"]
 option_dict["solver"] = ["Gurobi", "Xpress"]
 
 function unpack_config_struct(config, config_field, option_dict)
@@ -51,10 +87,6 @@ function unpack_config_struct(config, config_field, option_dict)
     end
 end
 
-# Parse inputs
-config_fp = joinpath(base_dir, "scripts", "configs", "hem_config.yaml")
-config = YAML.load_file(config_fp)
-
 bal_areas = option_dict["balancing_areas"]
 base_year = unpack_config_struct(config, "base_year", option_dict)
 num_future_years = unpack_config_struct(config, "num_future_years", option_dict)
@@ -67,28 +99,8 @@ rate_design = unpack_config_struct(config, "rate_design", option_dict)
 net_metering_policy = unpack_config_struct(config, "net_metering_policy", option_dict)
 solver = unpack_config_struct(config, "solver", option_dict)
 
-# Define scenario
-bal_areas_len = length(bal_areas)
-future_years = [base_year + i for i in range(1, num_future_years)]
-scenario = HEMDataRepo.DataSelection(bal_areas, base_year, future_years, num_ipps)
 
-# need to run in julia: run(output_dir = PROFILES_DIRECTORY, user = "nguo", hostname = HOSTNAME, dbname = DATABASE, port = PORT, pca_ids = nothing) to get residential and commercial profiles
-# also need to run in command prompt: python inputs/write_industrial_profiles.py #ba to get industrial profiles
 
-input_dir_name =
-    "ba_" *
-    "$bal_areas_len" *
-    "_base_" *
-    "$base_year" *
-    "_future_" *
-    "$num_future_years" *
-    "_ipps_" *
-    "$num_ipps" *
-    "_enhanced_test_full"
-input_dir = joinpath(hem_data_dir, "runs", input_dir_name)
-# mkpath(input_dir)
-
-# HEMDataRepo.parse_inputs(input_path, input_dir, scenario)
 
 # Restructure HEM options from user/config file input
 # market structure
@@ -203,12 +215,12 @@ jump_model = []
 output_dir = run_hem(
     input_dir,
     hem_opts,
-    regulator_options=regulator_opts,
-    ipp_options=ipp_opts,
-    utility_options=utility_opts,
-    green_developer_options=green_developer_opts,
-    customers_options=customers_opts,
-    force=true,
-    jump_model=jump_model,
+    regulator_options = regulator_opts,
+    ipp_options = ipp_opts,
+    utility_options = utility_opts,
+    green_developer_options = green_developer_opts,
+    customers_options = customers_opts,
+    force = true,
+    jump_model = jump_model,
 )
 # ------------------------------------------------------------------------------
